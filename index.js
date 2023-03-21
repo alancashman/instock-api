8776f05493b2ec49cc7321fe6d556894f3fe22eb
Alan Cashman <nocabbage@gmail.com>
2023-03-21 14:47:23 -0700

    initial commit - establish basic routes

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const { v4: uuid } = require("uuid");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors({ origin: CORS_ORIGIN }));
+
+////////
+// WAREHOUSES ENDPOINTS
+
+// GET list of all warehouses
+app.get("/api/warehouses", (req, res) => {});
+
+// GET single warehouse
+app.get("/api/warehouses/:id", (req, res) => {});
+
+// POST to create new warehouse
+app.post("/api/warehouses", (req, res) => {});
+
+// PUT to edit single warehouse
+app.put("/api/warehouses/:id", (req, res) => {});
+
+// DELETE a single warehouse
+app.delete("/api/warehouses/:id", (req, res) => {});
+
+////////
+// INVENTORY ENDPOINTS
+
+// GET all inventory items
+app.get("/api/inventories", (req, res) => {});
+
+// GET single inventory item
+app.get("/api/inventories/:id", (req, res) => {});
+
+// GET inventory for specific warehouse
+app.get("/api/warehouses/:id/inventories", (req, res) => {});
+
+// POST to create new inventory item
+app.post("/api/inventories", (req, res) => {});
+
+// PUT to edit inventory item
+app.put("/api/inventories/:id", (req, res) => {});
+
+// DELETE single inventory item
+app.delete("/api/inventories/:id", (req, res) => {});
+
+// Start server
+app.listen(PORT, (req, res) => {
+  console.log(`Listening on port ${PORT}...`);
+});
