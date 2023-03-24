fda3917e3284e93955a2924a21cc3e6f6a0ce820
Masha <mariia.tabala@gmail.com>
2023-03-24 13:42:05 -0700

    declare a route to edit/update an existing warehouse

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,12 +4,13 @@ const warehouseController = require("../controllers/warehouseController");
 
 router.route("/")
     .get(warehouseController.index) // get all warehouses
-     // POST/ CREATE a new warehouse
+    // POST/ CREATE a new warehouse
     .post(warehouseController.addWarehouse);
 
 router
-  .route("/:id")
-  .get(warehouseController.singleWarehouse)
-  .delete(warehouseController.deleteWarehouse);
+    .route("/:id")
+    .get(warehouseController.singleWarehouse)
+    .put(warehouseController.updateWarehouse)
+    .delete(warehouseController.deleteWarehouse);
 
 module.exports = router;
