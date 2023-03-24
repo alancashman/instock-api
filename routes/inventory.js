4d4a9b76fc0e2fee310de274d244087d1e185a02
MariaVan8 <fer_nandita_8@hotmail.com>
2023-03-24 11:04:10 -0700

    Declare the route for POST New Item

--- routes/inventory.js
+++ routes/inventory.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const inventoryController = require("../controllers/inventoryController");
 
-router.route("/").get(inventoryController.index); // get all inventory items
+router
+  .route("/")
+  .get(inventoryController.index) // get all inventory items
+  .post(inventoryController.addItem);
 
 router
   .route("/:id")
