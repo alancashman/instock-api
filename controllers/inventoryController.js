13b91e78fecd25305f4252851900b0a1f3d1447a
MariaVan8 <fer_nandita_8@hotmail.com>
2023-03-25 08:42:45 -0700

    Connect server with client

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
+const { v4: uuid } = require("uuid");
 
 // GET all inventory items
 exports.index = (_req, res) => {
@@ -47,5 +48,73 @@ exports.deleteItem = (req, res) => {
       res
         .status(404)
         .send(`Could not delete inventory item ${req.params.id}.  ${err}`);
+    });
+};
+
+//POST single inventory item
+exports.addItem = (req, res) => {
+  console.log("addItem", req.body);
+  let id = uuid();
+  // Validate the request body for required data
+  if (
+    !req.body.warehouse ||
+    !req.body.itemName ||
+    !req.body.description ||
+    !req.body.category ||
+    !req.body.status ||
+    !req.body.quantity
+  ) {
+    console.log("not working");
+    return res
+      .status(400)
+      .send(
+        `Please make sure to provide , warehouse ID, name, description,category,status and quantity`
+      );
+  }
+
+  const newItem = {
+    id: id,
+    warehouse_id: req.body.warehouse,
+    item_name: req.body.itemName,
+    description: req.body.description,
+    category: req.body.category,
+    status: req.body.status,
+    quantity: req.body.quantity,
+  };
+
+  // Check if the warehouse_id exists in the warehouses table
+  knex("warehouses")
+    .where("id", req.body.warehouse)
+    .first()
+    .then((warehouse) => {
+      console.log("warehouse exist: ", warehouse);
+      if (!warehouse) {
+        return res
+          .status(400)
+          .send(
+            `The warehouse_id '${req.body.warehouse_id}' does not exist in the warehouses table.`
+          );
+      }
+
+      // Check if quantity is a number
+      if (isNaN(req.body.quantity)) {
+        return res.status(400).send("The quantity value must be a number.");
+      }
+
+      console.log("before insert:", newItem);
+      // Insert new inventory item into the database
+      knex("inventories")
+        .insert(newItem)
+        // .returning("*")
+        .then((data) => {
+          console.log("data: ", data);
+          // For POST requests we need to respond with 201 and the newly created record
+          const newItemUrl = `/inventories/${data[0]}`;
+          res.status(201).json(newItemUrl);
+        })
+        .catch((err) => {
+          console.log("err: ", err);
+          res.status(400).send(`Error creating inventory item: ${err}`);
+        });
     });
 };
